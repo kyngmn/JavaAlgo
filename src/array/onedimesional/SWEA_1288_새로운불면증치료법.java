5c5e6314e52eeb45dee7c9969c3065a6979b9944
kyngmn <kmoffi@naver.com>
2025-02-14 17:59:08 +0900

    feat: 보충 수업 과제 진행중

---
+++ src/array/onedimesional/SWEA_1288_새로운불면증치료법.java
@@ -0,0 +1,39 @@
+package array.onedimesional;
+
+import java.util.Scanner;
+
+public class SWEA_1288_새로운불면증치료법 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+
+			int max = 0;
+			int k = 1;
+			int zero = (int) '0';
+			int sum = 0;
+			while (max != 9) {
+				for (int i = 1; i <= k * N; i++) {
+					String s = Integer.toString(i);
+
+					sum++;
+					
+					for (char c : s.toCharArray()) {
+						int ctoi = (int) c;
+						
+						
+						if (ctoi - zero > max) {
+							max = ctoi - zero;
+							break;
+						}
+					}
+				}
+				k++;
+			}
+
+			System.out.println(sum);
+		}
+		sc.close();
+	}
+}
