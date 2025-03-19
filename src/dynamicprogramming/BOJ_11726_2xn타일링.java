5043080eaba4949c2e0312df99192fbc81b25dd2
kyngmn <kmoffi@naver.com>
2025-03-19 11:31:39 +0900

    feat: dp study

---
+++ src/dynamicprogramming/BOJ_11726_2xn타일링.java
@@ -0,0 +1,40 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11726_2xn타일링 {
+	public static int MOD = 10007;
+	public static int[] memo;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		memo = new int[N + 1];
+
+		// 2*1을 넣을때 c는 무조건 1개, 1 증가
+		// 1*2을 넣을때 c는 무조건 2개, 2 증가
+		int res = pb(N);
+
+		System.out.println(res);
+	}
+
+	/**
+	 * 
+	 * @param n
+	 */
+	public static int pb(int n) {
+		if (n == 1) {
+			return memo[1] = 1;
+		}
+		if (n == 2) {
+			return memo[2] = 2;
+		}
+		// 불러오지 않으면 memo를 안쓰는 꼴
+		if (memo[n] > 0) {
+			return memo[n];
+		}
+		return memo[n] = (pb(n - 1) % MOD + pb(n - 2) % MOD) % MOD;
+	}
+}
---
+++ src/dynamicprogramming/BOJ_9095_123더하기.java
@@ -0,0 +1,44 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9095_123더하기 {
+	public static int[] onetwothree = { 1, 2, 3 };
+	public static int N;
+	public static int num;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int tc = 0;
+		while (tc++ < T) {
+			num = 0;
+			// 0 ~ 11
+			N = Integer.parseInt(br.readLine());
+
+			dp(0);
+
+			System.out.println(num);
+		}
+	}
+
+	/**
+	 * 
+	 * @param start 체크할 index
+	 * @param acc   누적합
+	 */
+	public static void dp(int acc) {
+		if (acc == N) {
+			num++;
+			return;
+		}
+		if (acc > N) {
+			return;
+		}
+		for (int i = 0; i < onetwothree.length; i++) {
+			dp(acc + onetwothree[i]);
+		}
+	}
+}
