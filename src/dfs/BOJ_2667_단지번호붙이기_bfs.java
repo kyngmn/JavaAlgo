f4eb4e4f9dddd2d477c79fb6bd11e1950967f778
kyngmn <kmoffi@naver.com>
2025-03-26 17:59:43 +0900

    feat: 햄스터 하는중..

---
+++ src/dfs/BOJ_2667_단지번호붙이기_bfs.java
@@ -0,0 +1,88 @@
+package dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_2667_단지번호붙이기_bfs {
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[][] delta = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 5~25
+		N = Integer.parseInt(br.readLine());
+		// 배열 초기화
+		map = new int[N + 1][N + 1];
+		visited = new boolean[N + 1][N + 1];
+
+		String str;
+		for (int r = 1; r <= N; r++) {
+			str = br.readLine();
+			for (int c = 1; c <= N; c++) {
+				map[r][c] = str.charAt(c - 1) == '1' ? 1 : 0;
+			}
+		}
+
+//		for (int i = 1; i < map.length; i++) {
+//			for (int j = 1; j < map[i].length; j++) {
+//				System.out.print(map[i][j]);
+//				System.out.print(" ");
+//			}
+//			System.out.println();
+//		}
+
+		List<Integer> list = new ArrayList<>();
+		Queue<int[]> q = new LinkedList<>();
+		int cnt = 0;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (!isValid(i, j))
+					continue;
+
+				cnt++;
+
+				int sum = 1;
+				q.add(new int[] { i, j });
+				visited[i][j] = true;
+
+				while (!q.isEmpty()) {
+					int[] curr = q.poll();
+
+					for (int di = 0; di < delta.length; di++) {
+						int nr = curr[0] + delta[di][0];
+						int nc = curr[1] + delta[di][1];
+						if (isValid(nr, nc)) {
+
+							q.add(new int[] { nr, nc });
+							visited[nr][nc] = true;
+							sum++;
+						}
+					}
+				}
+
+				list.add(sum);
+			}
+		}
+
+		System.out.println(cnt);
+		list.sort(null);
+		for (Integer x : list) {
+			System.out.println(x);
+		}
+	}
+
+	static boolean isValid(int row, int col) {
+		return row > 0 && row <= N && col > 0 && col <= N && !visited[row][col] && map[row][col] == 1;
+	}
+}
--- src/dfs/SWEA_8275_햄스터.java
+++ src/dfs/SWEA_8275_햄스터.java
@@ -3,17 +3,24 @@ package dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class SWEA_8275_햄스터 {
 	static int N;
-	static int[] cage;
-
 	static int X;
 	static int M;
-	static int[][] arr;
+	static int[][] note;
 
-	static final int max_ham = 60;
+	static int L;
+	static int R;
+
+	static int[] cage;
+	static int max;
+
+	static int currentNoteIndex;
+	static int currentL;
+	static int currentR;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,6 +35,9 @@ public class SWEA_8275_햄스터 {
 			// 햄스터 우리
 			N = Integer.parseInt(st.nextToken());
 			cage = new int[N + 1];
+			max = 0;
+			currentL = 0;
+			currentR = 0;
 
 			// 1~X,M~10
 			// X : 우리에 존재할 수 있는 최대 햄스터
@@ -38,23 +48,34 @@ public class SWEA_8275_햄스터 {
 			// "l번 우리에서 r번 우리까지의 햄스터 수를 세었더니 s마리였다."
 			M = Integer.parseInt(st.nextToken());
 
-			arr = new int[M][3];
-			int amount = Integer.MAX_VALUE;
+			note = new int[M][3];
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				// 1~l,r~N
-				int l = arr[i][0] = Integer.parseInt(st.nextToken());
-				int r = arr[i][1] = Integer.parseInt(st.nextToken());
+				int l = note[i][0] = Integer.parseInt(st.nextToken());
+				int r = note[i][1] = Integer.parseInt(st.nextToken());
 
 				// 0~60
-				int s = arr[i][2] = Integer.parseInt(st.nextToken());
-				int otherHamsterAmount = (N - (r - l + 1)) * X + s;
-				if (amount > otherHamsterAmount) {
-					amount = otherHamsterAmount;
+				int s = note[i][2] = Integer.parseInt(st.nextToken());
+
+				if (l < L) {
+					L = l;
+				}
+				if (r > R) {
+					R = r;
 				}
 			}
 
-			dfs(amount, 1);
+			currentL = note[0][0];
+			currentR = note[0][1];
+			dfs(0, cage);
+			System.out.println("잉" + Arrays.toString(cage));
+
+			for (int i = 1; i <= N; i++) {
+				if (i < L || i > R) {
+					cage[i] = X;
+				}
+			}
 
 			// 출력
 			StringBuilder sb = new StringBuilder();
@@ -63,16 +84,42 @@ public class SWEA_8275_햄스터 {
 		}
 	}
 
-	static void dfs(int amount, int currCageIndex) {
-		for (int i = 0; i < arr.length; i++) {
-			int l = arr[i][0];
-			int r = arr[i][1];
-			int s = arr[i][2];
+	static void dfs(int depth, int[] currentCage) {
+		int s = note[currentNoteIndex][2];
+		System.out.println(
+				"currentNoteIndex: " + currentNoteIndex + " , depth: " + depth + ": " + Arrays.toString(currentCage));
 
-			int temp = 0;
-			for (int j = l; j < r; j++) {
-				temp += cage[j];
+		if (depth == s) {
+			if (currentNoteIndex >= M - 1) {
+				if (depth > max) {
+					max = depth;
+					cage = Arrays.copyOf(currentCage, currentCage.length);
+				} else if (depth == max) {
+					if (Arrays.toString(cage).compareTo(Arrays.toString(currentCage)) > 0) {
+						cage = Arrays.copyOf(currentCage, currentCage.length);
+					}
+				}
+			} else {
+				currentNoteIndex++;
+				int nextDepth = 0;
+				for (int i = note[currentNoteIndex][0]; i <= note[currentNoteIndex][1]; i++) {
+					nextDepth += currentCage[i];
+				}
+				dfs(nextDepth, currentCage);
 			}
+			return;
+		}
+
+		int l = note[currentNoteIndex][0];
+		int r = note[currentNoteIndex][1];
+		for (int i = r; i >= l; i--) {
+			// 최대값이면 cage 키우지 않아
+			if (currentCage[i] >= X)
+				continue;
+			currentCage[i]++;
+//			System.out.println(depth + 1 + ": " + Arrays.toString(currentCage));
+			dfs(depth + 1, currentCage);
+			currentCage[i]--;
 		}
 	}
 }
